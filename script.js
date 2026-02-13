82491eb8513dc9ef8f6811a39cd5272993eb3352
mertcan-cyber <hulakirmertcan@gmail.com>
2026-02-13 15:01:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// YILDIZLAR
+// YILDIZ ANİMASYONU
 const canvas = document.getElementById("stars");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
@@ -31,61 +31,8 @@ document.getElementById("unlockBtn").onclick = () => {
     const input = document.getElementById("passwordInput").value;
 
     if (input === correctPassword) {
+
         document.getElementById("lockScreen").classList.add("hidden");
-        document.getElementById("siteContent").classList.remove("hidden");
-    } else {
-        document.getElementById("errorText").classList.remove("hidden");
-    }
-};
+        document.getElementById("afterUnlock").classList.remove("hidden");
 
-// FOTO SLAYT
-const slides = document.querySelectorAll("#slider img");
-let slideIndex = 0;
-slides[0].style.opacity = 1;
-
-setInterval(() => {
-    slides[slideIndex].style.opacity = 0;
-    slideIndex = (slideIndex + 1) % slides.length;
-    slides[slideIndex].style.opacity = 1;
-}, 3000);
-
-// BUTON OYUNU
-const yesBtn = document.getElementById("yesBtn");
-const noBtn = document.getElementById("noBtn");
-let noScale = 1;
-let yesScale = 1;
-
-noBtn.onclick = () => {
-
-    document.getElementById("warningText").classList.remove("hidden");
-
-    yesScale += 0.2;
-    yesBtn.style.transform = `scale(${yesScale})`;
-
-    noScale -= 0.2;
-
-    if (noScale <= 0) {
-        noBtn.style.display = "none";
-    } else {
-        noBtn.style.transform = `scale(${noScale})`;
-        noBtn.style.position = "absolute";
-        noBtn.style.left = Math.random() * (window.innerWidth - 100) + "px";
-        noBtn.style.top = Math.random() * (window.innerHeight - 50) + "px";
-    }
-};
-
-// FİNAL
-yesBtn.onclick = () => {
-
-    document.getElementById("siteContent").classList.add("hidden");
-    document.getElementById("finalScreen").classList.remove("hidden");
-
-    const text = "Sevgililer Günün Kutlu Olsun ❤️";
-    let i = 0;
-
-    const typing = setInterval(() => {
-        document.getElementById("finalText").innerHTML += text[i];
-        i++;
-        if (i >= text.length) clearInterval(typing);
-    }, 80);
-};
+        const text = "Kalbimin şifresini bulabileceğini biliyordum... ❤️\nHer zaman kalbimde bu şifre
