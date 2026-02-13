7ba0951f380daca840b6f7de5d9a616014b990f4
mertcan-cyber <hulakirmertcan@gmail.com>
2026-02-13 14:56:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// YILDIZ ANİMASYONU
+// YILDIZLAR
 const canvas = document.getElementById("stars");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
@@ -9,7 +9,7 @@ for (let i = 0; i < 200; i++) {
     stars.push({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
-        radius: Math.random() * 2
+        size: Math.random() * 2
     });
 }
 
@@ -18,17 +18,18 @@ function drawStars() {
     ctx.fillStyle = "white";
     stars.forEach(star => {
         ctx.beginPath();
-        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+        ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
         ctx.fill();
     });
 }
 setInterval(drawStars, 50);
 
 // ŞİFRE
-const correctPassword = "1234"; // BURAYI DEĞİŞTİR
+const correctPassword = "1234";
 
 document.getElementById("unlockBtn").onclick = () => {
     const input = document.getElementById("passwordInput").value;
+
     if (input === correctPassword) {
         document.getElementById("lockScreen").classList.add("hidden");
         document.getElementById("siteContent").classList.remove("hidden");
@@ -55,9 +56,12 @@ let noScale = 1;
 let yesScale = 1;
 
 noBtn.onclick = () => {
+
     document.getElementById("warningText").classList.remove("hidden");
+
     yesScale += 0.2;
     yesBtn.style.transform = `scale(${yesScale})`;
+
     noScale -= 0.2;
 
     if (noScale <= 0) {
@@ -65,18 +69,20 @@ noBtn.onclick = () => {
     } else {
         noBtn.style.transform = `scale(${noScale})`;
         noBtn.style.position = "absolute";
-        noBtn.style.left = Math.random() * window.innerWidth + "px";
-        noBtn.style.top = Math.random() * window.innerHeight + "px";
+        noBtn.style.left = Math.random() * (window.innerWidth - 100) + "px";
+        noBtn.style.top = Math.random() * (window.innerHeight - 50) + "px";
     }
 };
 
-// FİNAL SAHNESİ
+// FİNAL
 yesBtn.onclick = () => {
-    document.getElementById("questionSection").classList.add("hidden");
+
+    document.getElementById("siteContent").classList.add("hidden");
     document.getElementById("finalScreen").classList.remove("hidden");
 
     const text = "Sevgililer Günün Kutlu Olsun ❤️";
     let i = 0;
+
     const typing = setInterval(() => {
         document.getElementById("finalText").innerHTML += text[i];
         i++;
