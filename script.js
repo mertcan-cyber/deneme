83f5170677fea688278805a64ca8976086fe7654
mertcan-cyber <hulakirmertcan@gmail.com>
2026-02-13 16:37:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,38 @@ document.addEventListener("DOMContentLoaded", function () {
     unlockBtn.addEventListener("click", function () {
         console.log("Buton çalıştı");
 
-        lockScreen.style.display = "none";
-        mainSection.style.display = "flex";
+        lockScreen.classList.add("hidden");
+        mainSection.classList.remove("hidden");
+
+        initOtherFeatures(); // diğer şeyleri sonra başlat
     });
+
+    function initOtherFeatures() {
+
+        const yesBtn = document.getElementById("yesBtn");
+        const noBtn = document.getElementById("noBtn");
+
+        if (yesBtn && noBtn) {
+
+            let noScale = 1;
+            let yesScale = 1;
+
+            noBtn.addEventListener("click", function () {
+                yesScale += 0.2;
+                yesBtn.style.transform = `scale(${yesScale})`;
+                noScale -= 0.2;
+
+                if (noScale <= 0) {
+                    noBtn.style.display = "none";
+                } else {
+                    noBtn.style.transform = `scale(${noScale})`;
+                }
+            });
+
+            yesBtn.addEventListener("click", function () {
+                alert("Evet seçildi ❤️");
+            });
+        }
+    }
 
 });
